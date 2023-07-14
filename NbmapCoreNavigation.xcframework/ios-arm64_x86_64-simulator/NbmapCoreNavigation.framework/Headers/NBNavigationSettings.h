e15f1b4d90a0ae88d0b9a929faf956d844467019
qiu <zhengshen@nextbillion.ai>
2023-07-14 10:47:37 +0800

    Add cocoapods spec and xcframework

---
+++ NbmapCoreNavigation.xcframework/ios-arm64_x86_64-simulator/NbmapCoreNavigation.framework/Headers/NBNavigationSettings.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+/**
+ Posted when something changes in the shared `NBNavigationSettings` object.
+ 
+ The user info dictionary indicates which keys and values changed.
+ 
+ :nodoc:
+ */
+extern const NSNotificationName NBNavigationSettingsDidChangeNotification;
