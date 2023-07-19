25ce0e026f6a90a6f3f78ea81aac1d5fb1076581
qiu <zhengshen@nextbillion.ai>
2023-07-19 15:29:21 +0800

    Update framework to 1.1.4
    1. Update LICSECE
    2. Update pop spec version

--- NbmapCoreNavigation.xcframework/ios-arm64_x86_64-simulator/NbmapCoreNavigation.framework/Headers/NBRouteController.h
+++ NbmapCoreNavigation.xcframework/ios-arm64_x86_64-simulator/NbmapCoreNavigation.framework/Headers/NBRouteController.h
@@ -7,7 +7,7 @@
  
  :nodoc:
  */
-extern const NSNotificationName NBRouteControllerProgressDidChangeNotification;
+extern const NSNotificationName _Nullable NBRouteControllerProgressDidChangeNotification;
 
 /**
  Posted after the user diverges from the expected route, just before `NBRouteController` attempts to calculate a new route.
@@ -16,7 +16,7 @@ extern const NSNotificationName NBRouteControllerProgressDidChangeNotification;
  
  :nodoc:
  */
-extern const NSNotificationName NBRouteControllerWillRerouteNotification;
+extern const NSNotificationName _Nullable NBRouteControllerWillRerouteNotification;
 
 /**
  Posted when `NBRouteController` obtains a new route in response to the user diverging from a previous route.
@@ -25,7 +25,7 @@ extern const NSNotificationName NBRouteControllerWillRerouteNotification;
  
  :nodoc:
  */
-extern const NSNotificationName NBRouteControllerDidRerouteNotification;
+extern const NSNotificationName _Nullable NBRouteControllerDidRerouteNotification;
 
 /**
  Posted when `NBRouteController` fails to reroute the user after the user diverges from the expected route.
@@ -34,7 +34,7 @@ extern const NSNotificationName NBRouteControllerDidRerouteNotification;
  
  :nodoc:
  */
-extern const NSNotificationName NBRouteControllerDidFailToRerouteNotification;
+extern const NSNotificationName _Nullable NBRouteControllerDidFailToRerouteNotification;
 
 /**
  Posted when `NBRouteController` detects that the user has passed an ideal point for saying an instruction aloud.
@@ -43,9 +43,9 @@ extern const NSNotificationName NBRouteControllerDidFailToRerouteNotification;
  
  :nodoc:
  */
-extern const NSNotificationName NBRouteControllerDidPassSpokenInstructionPointNotification;
+extern const NSNotificationName _Nullable NBRouteControllerDidPassSpokenInstructionPointNotification;
 
-extern const NSNotificationName NBRouteControllerDidPassVisualInstructionPointNotification;
+extern const NSNotificationName _Nullable NBRouteControllerDidPassVisualInstructionPointNotification;
 
 /**
  Keys in the user info dictionaries of various notifications posted by instances of `NBRouteController`.
@@ -57,38 +57,38 @@ typedef NSString *NBRouteControllerNotificationUserInfoKey NS_EXTENSIBLE_STRING_
 /**
  A key in the user info dictionary of a `Notification.Name.NBRouteControllerProgressDidChange` or `Notification.Name.RouteControllerDidPassSpokenInstructionPoint` notification. The corresponding value is a `RouteProgress` object representing the current route progress.
  */
-extern const NBRouteControllerNotificationUserInfoKey NBRouteControllerRouteProgressKey;
+extern const NBRouteControllerNotificationUserInfoKey _Nullable NBRouteControllerRouteProgressKey;
 
 /**
  A key in the user info dictionary of a `Notification.Name.NBRouteControllerProgressDidChange` or `Notification.Name.RouteControllerWillReroute` notification. The corresponding value is a `CLLocation` object representing the current idealized user location.
  */
-extern const NBRouteControllerNotificationUserInfoKey NBRouteControllerLocationKey;
+extern const NBRouteControllerNotificationUserInfoKey _Nullable NBRouteControllerLocationKey;
 
 /**
  A key in the user info dictionary of a `Notification.Name.NBRouteControllerProgressDidChange` or `Notification.Name.RouteControllerWillReroute` notification. The corresponding value is a `CLLocation` object representing the current raw user location.
  */
-extern const NBRouteControllerNotificationUserInfoKey NBRouteControllerRawLocationKey;
+extern const NBRouteControllerNotificationUserInfoKey _Nullable NBRouteControllerRawLocationKey;
 
 /**
  A key in the user info dictionary of a `Notification.Name.RouteControllerDidFailToReroute` notification. The corresponding value is an `NSError` object indicating why `RouteController` was unable to calculate a new route.
  */
-extern const NBRouteControllerNotificationUserInfoKey NBRouteControllerRoutingErrorKey;
+extern const NBRouteControllerNotificationUserInfoKey _Nullable NBRouteControllerRoutingErrorKey;
 
 /**
  A key in the user info dictionary of a `Notification.Name.RouteControllerDidReroute` notification. The corresponding value is an `NSNumber` instance containing a Boolean value indicating whether `RouteController` proactively rerouted the user onto a faster route.
  */
-extern const NBRouteControllerNotificationUserInfoKey NBRouteControllerIsProactiveKey;
+extern const NBRouteControllerNotificationUserInfoKey _Nullable NBRouteControllerIsProactiveKey;
 
 /**
  A key in the user info dictionary of a `Notification.Name.routeControllerProgressDidChange` notification. The corresponding value is a `CLHeading` object representing the current user heading.
  */
-extern const NBRouteControllerNotificationUserInfoKey NBRouteControllerHeadingKey;
+extern const NBRouteControllerNotificationUserInfoKey _Nullable NBRouteControllerHeadingKey;
 
 
 /**
  A key in the user info dictionary of a `Notification.Name.speedDidChange` notification. The corresponding value is a `CLLocationSpeed` object representing the current user speed.
  */
-extern const NBRouteControllerNotificationUserInfoKey NBRouteControllerSpeedKey;
+extern const NBRouteControllerNotificationUserInfoKey _Nullable NBRouteControllerSpeedKey;
 
 
 @interface NSString (MD5)
